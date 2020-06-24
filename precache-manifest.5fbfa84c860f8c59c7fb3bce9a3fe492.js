780ce99f0dc23ed3d4b23409d3486057d0434f95
vivekpandey790 <v.b.pandey@accenture.com>
2020-06-24 14:59:43 +0530

    Updates

---
+++ precache-manifest.5fbfa84c860f8c59c7fb3bce9a3fe492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e8c7ca4e4cf7cfdaaf317c2db017231",
+    "url": "/react-learning/index.html"
+  },
+  {
+    "revision": "2e384713979b0373488d",
+    "url": "/react-learning/static/css/main.452af0bd.chunk.css"
+  },
+  {
+    "revision": "c28791846dc0b822be00",
+    "url": "/react-learning/static/js/2.c1bbeb44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-learning/static/js/2.c1bbeb44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e384713979b0373488d",
+    "url": "/react-learning/static/js/main.240c48f7.chunk.js"
+  },
+  {
+    "revision": "4e344dafd80d29a27581",
+    "url": "/react-learning/static/js/runtime-main.9e6f19ba.js"
+  }
+]);
